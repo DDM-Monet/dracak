5e524ac097201baf8d6775401497ba4af5e936d5
Michal Hrusecky <Michal@Hrusecky.net>
2017-03-29 18:26:39 +0200

    Umirani uz neni povinny

--- quest1.c
+++ quest1.c
@@ -4,5 +4,12 @@
 #include "hrdina.h"
 
 void zemri(struct hrdina *h) {
-    h->HP = -1;
+    int volba=0;
+    printf("Chces zemrit?\n");
+    printf(" 1 - ANO\n");
+    printf(" 2 - NE\n");
+    scanf("%d", &volba);
+    if(volba != 2) {
+        h->HP = -1;
+    }
 }

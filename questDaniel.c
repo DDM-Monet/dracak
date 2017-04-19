4abd2a60c3df131ea061d4fc872fdac6e47c02d8
Marek12345678CZE <marek.praha12@seznam.cz>
2017-04-19 18:04:36 +0200

    rozhodnuti koně

--- questDaniel.c
+++ questDaniel.c
@@ -6,11 +6,18 @@
 void chytit_kone(struct hrdina *h) {
 	int action = 0;
 	int stesti=10 + (random() % 11);
-	printf ("potkas divokeho kone. Ches utect(1) nebo ho zkrotit(2)?\n");
-	scanf ("%d", &action);
-	if (action == 2) {
-		printf ("nemel jsi dneska stesti\n [ztratil jsi stesti]\n");
-		stesti -= 1;
+	int i = 0;
+	while (i == 0){
+		printf ("potkas divokeho kone. Ches utect(1) nebo ho zkrotit(2)?\n");
+		scanf("%d", &action);
+		if (action == 2) {
+			printf ("nemel jsi dneska stesti\n [ztratil jsi stesti]\n");
+			h->stesti -= 1;
+			i = 1;
+		}else{
+			printf("Uspěšně jsi utekl,\n");
+			printf("ale jseš srab.\n");
+			printf("zkus to znovu!!!!!!!!!!!!!!!!!!!!!.\n");
+		}
 	}
-}
-		
+}		
